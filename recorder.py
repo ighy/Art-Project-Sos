a100f88e3c15060d43ee750a8f7a4f8450819b8d
NeonFuzion <derekchang0210@gmail.com>
2025-05-19 14:05:04 -0700

    Tkinter Integration Progress

--- recorder.py
+++ recorder.py
@@ -1,40 +1,62 @@
-import server
-import RPi.GPIO as gpio
-from recorder import Recorder
+import pyaudio
+import wave
+from tkinter import *
+from tkinter import ttk
 
-gpio.setmode (gpio.BCM)
-
-class ButtonRecorder (object):
-    def __init__(self, filename):
-        self.filename = filename
-        gpio.setup(23, gpio.IN, pull_up_down=gpio. PUD_UP)
-        self.rec = Recorder (channels=2)
-
-    def start(self):
-        gpio.add_event_detect (23, gpio. FALLING, callback=self.falling, bouncetime=10)
-
-    def rising (self, channel):
-        gpio. remove_event_detect (23)
-        print('Button up')
-        gpio.add_event_detect (23, gpio. FALLING, callback=self.falling, bouncetime=10)
-        self.recfile.stop_recording()
-        self.recfile.close()
-
-    def falling (self, channel):
-        gpio. remove_event_detect (23)
-        print('Button down')
-        gpio.add_event_detect (23, gpio. RISING, callback=self. rising, bouncetime=10)
-        self.recfile = self.rec.open(self.filename, 'wb')
-        self.recfile.start_recording()
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Record Your Dream").grid(column=0, row=0)
+ttk.Button(frm, text="Start Recording", command=root.destroy).grid(column=1, row=1)
+ttk.Button(frm, text="Stop Recording", command=root.destroy).grid(column=0, row=1)
+root.mainloop()
 
 index = 0
+recording = False
 while True:
+    # the file name output you want to record into
+    filename = f"dream_{index}.wav"
     index += 1
-    rec = ButtonRecorder(f"dream{index}.wav")
-    rec.start()
-
-    try:
-        raw_input()
-
-    except KeyboardInterrupt: break
-gpio.cleanup()
+    # set the chunk size of 1024 samples
+    chunk = 1024
+    # sample format
+    FORMAT = pyaudio.paInt16
+    # mono, change to 2 if you want stereo
+    channels = 1
+    # 44100 samples per second
+    sample_rate = 44100
+    # initialize PyAudio object
+    p = pyaudio.PyAudio()
+    # open stream object as input & output
+    stream = p.open(format=FORMAT,
+                    channels=channels,
+                    rate=sample_rate,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=chunk)
+    frames = []
+    print("Recording...")
+    while recording:
+        data = stream.read(chunk)
+        # if you want to hear your voice while recording
+        # stream.write(data)
+        frames.append(data)
+    print("Finished recording.")
+    # stop and close stream
+    stream.stop_stream()
+    stream.close()
+    # terminate pyaudio object
+    p.terminate()
+    # save audio file
+    # open the file in 'write bytes' mode
+    wf = wave.open(filename, "wb")
+    # set the channels
+    wf.setnchannels(channels)
+    # set the sample format
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    # set the sample rate
+    wf.setframerate(sample_rate)
+    # write the frames as bytes
+    wf.writeframes(b"".join(frames))
+    # close the file
+    wf.close()
