96cbd224942a7b51cad5bcae8653a78a39b37dde
NeonFuzion <derekchang0210@gmail.com>
2025-05-20 13:49:49 -0700

    Virtual Environment Nonsense

--- client_venv/recorder.py
+++ client_venv/recorder.py
@@ -1,62 +1,68 @@
+# Import the necessary modules.
+import tkinter
+import tkinter as tk
+import tkinter.messagebox
 import pyaudio
 import wave
-from tkinter import *
-from tkinter import ttk
+import os
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Record Your Dream").grid(column=0, row=0)
-ttk.Button(frm, text="Start Recording", command=root.destroy).grid(column=1, row=1)
-ttk.Button(frm, text="Stop Recording", command=root.destroy).grid(column=0, row=1)
-root.mainloop()
 
-index = 0
-recording = False
-while True:
-    # the file name output you want to record into
-    filename = f"dream_{index}.wav"
-    index += 1
-    # set the chunk size of 1024 samples
-    chunk = 1024
-    # sample format
-    FORMAT = pyaudio.paInt16
-    # mono, change to 2 if you want stereo
-    channels = 1
-    # 44100 samples per second
-    sample_rate = 44100
-    # initialize PyAudio object
-    p = pyaudio.PyAudio()
-    # open stream object as input & output
-    stream = p.open(format=FORMAT,
-                    channels=channels,
-                    rate=sample_rate,
-                    input=True,
-                    output=True,
-                    frames_per_buffer=chunk)
-    frames = []
-    print("Recording...")
-    while recording:
-        data = stream.read(chunk)
-        # if you want to hear your voice while recording
-        # stream.write(data)
-        frames.append(data)
-    print("Finished recording.")
-    # stop and close stream
-    stream.stop_stream()
-    stream.close()
-    # terminate pyaudio object
-    p.terminate()
-    # save audio file
-    # open the file in 'write bytes' mode
-    wf = wave.open(filename, "wb")
-    # set the channels
-    wf.setnchannels(channels)
-    # set the sample format
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    # set the sample rate
-    wf.setframerate(sample_rate)
-    # write the frames as bytes
-    wf.writeframes(b"".join(frames))
-    # close the file
-    wf.close()
+class RecAUD:
+
+    def __init__(self, chunk=3024, frmat=pyaudio.paInt16, channels=2, rate=44100, py=pyaudio.PyAudio()):
+
+        # Start Tkinter and set Title
+        self.main = tkinter.Tk()
+        self.collections = []
+        self.main.geometry('500x300')
+        self.main.title('Record')
+        self.CHUNK = chunk
+        self.FORMAT = frmat
+        self.CHANNELS = channels
+        self.RATE = rate
+        self.p = py
+        self.frames = []
+        self.st = 1
+        self.stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK)
+
+        # Set Frames
+        self.buttons = tkinter.Frame(self.main, padx=120, pady=20)
+
+        # Pack Frame
+        self.buttons.pack(fill=tk.BOTH)
+
+
+
+        # Start and Stop buttons
+        self.strt_rec = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Start Recording', command=lambda: self.start_record())
+        self.strt_rec.grid(row=0, column=0, padx=50, pady=5)
+        self.stop_rec = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Stop Recording', command=lambda: self.stop())
+        self.stop_rec.grid(row=1, column=0, columnspan=1, padx=50, pady=5)
+
+        tkinter.mainloop()
+
+    def start_record(self):
+        self.st = 1
+        self.frames = []
+        stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK)
+        while self.st == 1:
+            data = stream.read(self.CHUNK)
+            self.frames.append(data)
+            print("* recording")
+            self.main.update()
+
+        stream.close()
+
+        wf = wave.open('test_recording.wav', 'wb')
+        wf.setnchannels(self.CHANNELS)
+        wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
+        wf.setframerate(self.RATE)
+        wf.writeframes(b''.join(self.frames))
+        wf.close()
+
+    def stop(self):
+        self.st = 0
+
+
+# Create an object of the ProgramGUI class to begin the program.
+guiAUD = RecAUD()
