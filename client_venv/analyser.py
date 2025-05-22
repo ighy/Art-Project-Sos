fba78af034dd385b81bac777f35089103b5b2b20
NeonFuzion <106327057+NeonFuzion@users.noreply.github.com>
2025-05-22 07:07:28 -0700

    Analyser UI Progress

--- client_venv/analyser.py
+++ client_venv/analyser.py
@@ -1,3 +1,4 @@
+from functools import partial
 import tkinter
 import tkinter as tk
 import tkinter.messagebox
@@ -14,7 +15,7 @@ class Analyser:
 
         # Other
         self.column = 0
-        self.row = 0
+        self.row = 1
         self.filename = ''
 
         # Set Frames
@@ -25,22 +26,22 @@ class Analyser:
         self.buttons.pack(fill=tk.BOTH)
 
         # Creating Buttons
-        self.start_button = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Play', command=lambda: self.play_clip())
+        self.start_button = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Play', command=lambda: self.play_clip)
         self.start_button.grid(row=0, column=0, padx=50, pady=5)
-        self.stop_button = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Stop', command=lambda: self.stop_clip())
+        self.stop_button = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Stop', command=lambda: self.stop_clip)
         self.stop_button.grid(row=0, column=1, columnspan=1, padx=50, pady=5)
-        self.approve_button = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Approve', command=lambda: self.approve_clip())
+        self.approve_button = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Approve', command=lambda: self.approve_clip)
         self.approve_button.grid(row=0, column=2, padx=50, pady=5)
-        self.flag_button = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Flag', command=lambda: self.flag_clip())
+        self.flag_button = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Flag', command=lambda: self.flag_clip)
         self.flag_button.grid(row=0, column=3, columnspan=1, padx=50, pady=5)
-        tkinter.mainloop()
+        #tkinter.mainloop()
 
     def add_file(self, filename):
-        clip = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text=filename, command=lambda: self.select_clip(filename))
+        clip = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text=filename, command=partial(self.select_clip, filename))
         clip.grid(row=self.row, column=self.column, padx=50, pady=5)
-        y += 1
-        cur_scolumn += 1
+        self.column += 1
         self.buttonList.append(clip)
+        tkinter.mainloop()
 
     def select_clip(self, filename):
         self.filename = filename
